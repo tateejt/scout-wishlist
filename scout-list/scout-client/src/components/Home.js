0ac218008988dd6e6f0405c16e43e56b926d3793
tateejt <tateejt@gmail.com>
2020-03-17 17:55:42 -0700

    Create Home.js

---
+++ scout-list/scout-client/src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../css/App.css';
+
+
+const Home = () => {
+    return (
+        <div className="container" id="home">
+            <h1>Scout Wishlist </h1>
+            <br />
+            <h2>Athletes to Scout</h2>
+
+            <span>
+                NFL E. Tate
+
+            </span>
+        </div>
+    )
+}
+export default Home;
