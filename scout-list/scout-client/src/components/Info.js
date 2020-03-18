e8c3102e59775b9a49c2570cdb314e3f50623409
tateejt <tateejt@gmail.com>
2020-03-17 17:55:47 -0700

    Create Info.js

---
+++ scout-list/scout-client/src/components/Info.js
@@ -0,0 +1,23 @@
+
+import React from 'react'
+
+
+ const Info = () => {
+    return (
+        <div className="info">
+            <h2> Resources for the NFL Scouting</h2><br/>
+
+            <p><span>This app to help those scouts gather a complete list of which may make their team better in the long term.
+            You will be able to add players to a list and get the general information about them and some of their scouting accomplishments. I hope this app has
+          helped you become a better scout and maybe more organized.</span></p>
+
+                <hr/>
+
+            <h3> NFL Scouting Report -</h3>
+            <p> http://www.nfl.com/combine </p>
+            <h3>NFL mock draft - </h3>
+            <p>https://www.cbssports.com/nfl/draft/news/three-round-2020-nfl-mock-draft-dolphins-pass-on-tua-tagovailoa-but-go-qb-later-colts-bucs-find-day-2-qbs/</p>
+        </div>
+    )
+}
+export default Info;
